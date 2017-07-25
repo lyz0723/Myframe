9c6680580b698542fc162743cef26d66e1f5ca25
lyz0723 <360196045@qq.com>
2017-07-25 22:09:29 +0800

    dd

---
+++ application/models/ItemModel.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/25
+ * Time: 21:35
+ */
+class ItemModel extends Model
+{
+    /* 业务逻辑层实现 */
+}
---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/25
+ * Time: 21:29
+ */
+/** 变量配置 **/
+define('DB_NAME', 'test');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_HOST', 'localhost');
